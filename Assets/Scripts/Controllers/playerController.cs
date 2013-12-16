794e619aa0a62c4dcf224904756527a6c28ed367
Brendan Ritter <brendan.ritter@yahoo.com>
2013-12-16 00:02:06 -0500

    Fixed some noclipping problems with horizontal movement.
    This was done by increasing the number of raycasts per fixed update by
    two. In addition, both the top and bottom are checked now, not just the
    middle. Finally, during testing, it is clear that vertical ladder
    movement needs to be fixed. STABLE

--- Assets/Scripts/Controllers/playerController.cs
+++ Assets/Scripts/Controllers/playerController.cs
@@ -5,11 +5,94 @@ public class playerController : MonoBehaviour
 {
 
 		public float m_speed;
-		public Transform left;
-		public Transform right;
+		public Transform leftHigh;
+		public Transform leftLow;
+		public Transform rightHigh;
+		public Transform rightLow;
 		//public int m_mineStr;
 		// Use this for initialization
-		void MoveHoriz ()
+		void MoveHorizNew ()
+		{
+				float rayScale = .25F;
+				float horizDir = Input.GetAxis ("Horizontal");
+				//Debug.Log (horizDir);
+				//float vertDir = Input.GetAxis ("Vertical");
+			
+				RaycastHit2D infoHLeft;
+				RaycastHit2D infoLLeft;
+				RaycastHit2D infoHRight;
+				RaycastHit2D infoLRight;
+				
+				float closeLeftDiff;
+				float closeRightDiff;
+				RaycastHit2D closeLeftInfo;
+				RaycastHit2D closeRightInfo;
+			
+				Vector3 leftHPos = leftHigh.position - (new Vector3 (0, .01f, 0));
+				Vector3 leftLPos = leftLow.position + (new Vector3 (0, .01f, 0));
+				Vector3 rightHPos = rightHigh.position - (new Vector3 (0, .01f, 0));
+				Vector3 rightLPos = rightLow.position + (new Vector3 (0, .01f, 0));
+			
+				infoHLeft = Physics2D.Raycast (new Vector2 (leftHPos.x, leftHPos.y), -Vector2.right, m_speed * rayScale);
+				infoHRight = Physics2D.Raycast (new Vector2 (rightHPos.x, rightHPos.y), Vector2.right, m_speed * rayScale);
+				infoLLeft = Physics2D.Raycast (new Vector2 (leftLPos.x, leftLPos.y), -Vector2.right, m_speed * rayScale);
+				infoLRight = Physics2D.Raycast (new Vector2 (rightLPos.x, rightLPos.y), Vector2.right, m_speed * rayScale);			
+				//Debug.DrawLine (new Vector3 (xPosRight, yPos, 0), new Vector3 (xPosRight, yPos - gravSize, 0), Color.blue);
+				Vector3 amountMove = (new Vector3 (horizDir, 0f, 0f) * m_speed * Time.fixedDeltaTime);
+				//Vector3 amountMove = (new Vector3 (.1f, 0f, 0f));
+				//Debug.Log ("Amountmove: " + amountMove);
+				float leftHDiff = Mathf.Abs (leftHPos.x - infoHLeft.point.x);
+				float rightHDiff = Mathf.Abs (rightHPos.x - infoHRight.point.x);
+				float leftLDiff = Mathf.Abs (leftLPos.x - infoLLeft.point.x);
+				float rightLDiff = Mathf.Abs (rightLPos.x - infoLRight.point.x);
+			
+
+				if (leftHDiff > leftLDiff) {
+						closeLeftDiff = leftLDiff;
+						closeLeftInfo = infoLLeft;
+				} else {
+						closeLeftDiff = leftHDiff;
+						closeLeftInfo = infoHLeft;
+				}
+				if (rightHDiff > rightLDiff) {
+						closeRightDiff = rightLDiff;
+						closeRightInfo = infoLRight;
+				} else {
+						closeRightDiff = rightHDiff;
+						closeRightInfo = infoHRight;
+				}
+				
+			
+				if ((closeRightInfo.collider != null) && (horizDir > 0) && (closeRightDiff < Mathf.Abs (amountMove.x))) {
+						if (! closeRightInfo.collider.gameObject.GetComponent<permeability> ().m_horizontal) {
+								//Debug.Log ("RightIf" + closeRightDiff);
+								transform.position += (new Vector3 (closeRightDiff, 0, 0));
+						} else {
+								//Debug.Log ("Right");
+								transform.position += amountMove;
+						}
+						Debug.Log (closeRightInfo.collider.gameObject.name);
+						//Debug.DrawLine (rightHPos, rightHPos + (new Vector3 (m_speed * rayScale, 0, 0)), Color.green);
+				} else if ((closeLeftInfo.collider != null) && (horizDir < 0) && (closeLeftDiff < Mathf.Abs (amountMove.x))) {
+						if (! closeLeftInfo.collider.gameObject.GetComponent<permeability> ().m_horizontal) {
+								transform.position -= (new Vector3 (closeLeftDiff, 0, 0));
+								//Debug.Log ("LeftIf: " + closeLeftDiff);
+						} else {
+								//Debug.Log ("Left");
+								transform.position += amountMove;
+						}
+						Debug.Log (closeLeftInfo.collider.gameObject.name);
+						//Debug.Log ("Left Diff" + leftDiff);
+						//Debug.Log ("Amount Move" + Mathf.Abs (amountMove.x));
+						//Debug.DrawLine (leftPos, leftPos - (new Vector3 (m_speed * rayScale, 0, 0)), Color.green);
+				} else {
+						//Debug.Log ("else: " + amountMove);
+						transform.position += amountMove;
+				}
+		
+				//MineLogic (infoRight, infoLeft, leftDiff, rightDiff, horizDir);
+		}	
+		/*void MoveHoriz ()
 		{
 				float rayScale = .25F;
 				float horizDir = Input.GetAxis ("Horizontal");
@@ -51,7 +134,7 @@ public class playerController : MonoBehaviour
 				}
 				
 				//MineLogic (infoRight, infoLeft, leftDiff, rightDiff, horizDir);
-		}
+		}*/
 		/*
 		void MineLogic (RaycastHit2D infoRight, RaycastHit2D infoLeft, float leftDiff, float rightDiff, float horizDir)
 		{	
@@ -79,7 +162,8 @@ public class playerController : MonoBehaviour
 		}
 		void FixedUpdate ()
 		{
-				MoveHoriz ();
+				//Debug.Log (Input.GetAxis ("Horizontal"));
+				MoveHorizNew ();
 				//Gravity ();
 		}
 }
