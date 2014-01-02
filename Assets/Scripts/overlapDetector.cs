89eeee1a2823137bc05344e30990818be1c86d74
Brendan Ritter <brendan.ritter@yahoo.com>
2014-01-02 18:19:03 -0500

    Added a check to see if the player is inside another object.
    Maybe should replace the circle cast in the ladderController with this
    check. Will be used for interaction with buildings. Also added a new
    placeholder factory sprite to the sheet and scene.

---
+++ Assets/Scripts/overlapDetector.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class overlapDetector : MonoBehaviour
+{
+
+	private Hashtable m_olapObjs;
+	public float m_checkRange;
+	private bool m_check = true;
+	void Start ()
+	{
+		m_olapObjs = new Hashtable ();
+	}
+	// Update is called once per frame
+	void Update ()
+	{	
+		if (m_check) {
+			m_check = false;
+			//Debug.Log ("Inside");
+			StartCoroutine (checkOverlap ());
+			//checkOverlap ();
+		}
+	}
+	IEnumerator checkOverlap ()
+	{
+		yield return new WaitForSeconds (0.5f);
+		m_check = true;
+		Collider2D[] coll = new Collider2D[5];
+		float scale = m_checkRange;
+		int numHit = Physics2D.OverlapCircleNonAlloc ((Vector2)transform.position, scale, coll, Physics.AllLayers);
+		for (int i=0; i<numHit; i++) {
+			if (!m_olapObjs.ContainsKey (coll [i].gameObject)) {
+				m_olapObjs.Add (coll [i].gameObject, null);
+				Debug.Log (coll [i].gameObject);
+			}
+		}
+	}
+}
