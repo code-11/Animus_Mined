e274293973b2b67adb3c2abe26d9bb35589410be
Brendan Ritter <brendan.ritter@yahoo.com>
2014-01-22 01:25:27 -0500

    Made some stylistic changes to the inventory.
    Is now a bottom bar mac style, rather than an annoying screen filling
    one. Need to handle overflow and selection still.

--- Assets/InventoryGui.cs
+++ Assets/InventoryGui.cs
@@ -4,19 +4,18 @@ using System.Collections;
 public class InventoryGui : MonoBehaviour
 {
 		public Texture2D btnTexture;
-		
 		void OnGUI ()
 		{
-				GUI.Box (new Rect (10, 10, 300, 300), "Inventory");
+				GUI.Box (new Rect (170, Screen.height - 110, 1000, 100), "Inventory");
 				inventoryManager manager = gameObject.GetComponent<inventoryManager> ();
-				int x = 30;
-				int y = 30;
+				int x = 190;
+				int y = Screen.height - 90;
 				
 				
 				foreach (var item in manager.getInven().Values) {
 						if (GUI.Button (new Rect (x, y, 50, 50), btnTexture))
 								Debug.Log ("Clicked the button with an image");
-						GUI.Label (new Rect (x + 10, y + 30, 50, 50), ((GameObject)item).name);
+						GUI.Label (new Rect (x, y + 45, 50, 50), ((GameObject)item).name);
 						x = x + 60;
 				}
 
