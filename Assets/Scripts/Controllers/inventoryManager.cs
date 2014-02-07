2284c9521bc374a0a5c2e27e90aca442574b4bb3
Brendan Ritter <brendan.ritter@yahoo.com>
2014-02-07 15:02:52 -0500

    Converted the inventory in InventoryManager from a hash table to an ArrayList.
    Still generates errors, but doesn't seem to affect performance. Strange.

--- Assets/Scripts/Controllers/inventoryManager.cs
+++ Assets/Scripts/Controllers/inventoryManager.cs
@@ -4,136 +4,138 @@ using System.Collections;
 public class inventoryManager : MonoBehaviour
 {
 
-		/*
+	/*
 		m_inventory is a hashtable that remembers items in the player's inventory
 		Key: inventory number, ie which slot it is in.
 		Value: the gameobject itself.
 		*/
-		private Hashtable m_inventory;
-		//public GameObject m_placeholder;
-		private int invenNum = 0;
-		private bool invenUp = false;
-		public int selected = 0;
+	private ArrayList m_inventory;
+	//public GameObject m_placeholder;
+	private int invenNum = 0;
+	private bool invenUp = false;
+	private int m_numSelected = 0;
 		
-		public GameObject getSelObj ()
-		{
-				return (GameObject)m_inventory [selected];
-		}
-		public void setSelNum (int newNum)
-		{
-				selected = newNum;
-		}
-		public int getSelNum ()
-		{
-				return selected;
-		}
-		void Start ()
-		{
+	public GameObject getSelObj ()
+	{
+		return (GameObject)m_inventory [m_numSelected];
+	}
+	public void setSelNum (int newNum)
+	{
+		m_numSelected = newNum;
+	}
+	public int getSelNum ()
+	{
+		return m_numSelected;
+	}
+	void Start ()
+	{
 /*				GameObject m_startItems[]={};*/
-				m_inventory = new Hashtable ();
+		m_inventory = new ArrayList ();
 /*				foreach (var item in m_startItems) {
 						m_inventory.Add (m_inventory.Count, item);
 				}*/
-		}
-		void Update ()
-		{
-				runInven ();
-		}
+	}
+	void Update ()
+	{
+		runInven ();
+	}
 		
-		void shiftLeft ()
-		{
-				if (selected > 0) {
-						selected -= 1;
-				}
+	void shiftLeft ()
+	{
+		if (m_numSelected == 0) {
+			m_numSelected = 0;
+		} else {
+			m_numSelected -= 1;
 		}
-		void shiftRight ()
-		{
-				if (selected < invenNum) {
-						selected += 1;
-				}
+	}
+	void shiftRight ()
+	{
+		if (m_numSelected < m_inventory.Count - 1) {
+			m_numSelected += 1;
 		}
+	}
 		
-		void runInven ()
-		{
-				InventoryGui invenGui = gameObject.GetComponent<InventoryGui> ();
-				bool iPress = Input.GetKeyDown ("i");
-				bool selLeft = Input.GetKeyDown ("q");
-				bool selRight = Input.GetKeyDown ("e");
-				bool rPress = Input.GetKeyDown ("r");
-				bool pPress = Input.GetKeyDown ("p");
-				if (pPress) {
-						RemoveSelItem ();
-				}
-				if (rPress) {
-						UseSelected ();
-				}
-				if (selLeft) {
-						shiftLeft ();
-				}
-				if (selRight) {
-						shiftRight ();
-				}
-				if (iPress) {
-						if (invenUp == true) {
-								invenUp = false;
-								invenGui.enabled = false;
-						} else {
-								invenUp = true;
-								invenGui.enabled = true;
-						}
-						//StartCoroutine (viewIven ());
-				}
-				pickUp ();				
-		}	
-		public Hashtable getInven ()
-		{
-				return m_inventory;
+	void runInven ()
+	{
+		InventoryGui invenGui = gameObject.GetComponent<InventoryGui> ();
+		bool iPress = Input.GetKeyDown ("i");
+		bool selLeft = Input.GetKeyDown ("q");
+		bool selRight = Input.GetKeyDown ("e");
+		bool rPress = Input.GetKeyDown ("r");
+		bool pPress = Input.GetKeyDown ("p");
+		if (pPress) {
+			RemoveSelItem ();
 		}
-		void pickUp ()
-		{
-				overlapDetector waitItems = gameObject.GetComponent<overlapDetector> ();
-				if (waitItems != null) {
-						if (waitItems.m_olapObjs.ContainsKey ("PickUp")) {
-								GameObject theObj = (GameObject)waitItems.m_olapObjs ["PickUp"];
-								AddItem (theObj);
-								theObj.SetActive (false);
-								waitItems.m_olapObjs.Remove ("PickUp");
-						}
-				}
+		if (rPress) {
+			UseSelected ();
 		}
-		public void AddItem (GameObject item)
-		{
-				if (!m_inventory.ContainsValue (item)) {
+		if (selLeft) {
+			shiftLeft ();
+		}
+		if (selRight) {
+			shiftRight ();
+		}
+		if (iPress) {
+			if (invenUp == true) {
+				invenUp = false;
+				invenGui.enabled = false;
+			} else {
+				invenUp = true;
+				invenGui.enabled = true;
+			}
+			//StartCoroutine (viewIven ());
+		}
+		pickUp ();				
+	}	
+	public ArrayList getInven ()
+	{
+		return m_inventory;
+	}
+	void pickUp ()
+	{
+		overlapDetector waitItems = gameObject.GetComponent<overlapDetector> ();
+		if (waitItems != null) {
+			if (waitItems.m_olapObjs.ContainsKey ("PickUp")) {
+				GameObject theObj = (GameObject)waitItems.m_olapObjs ["PickUp"];
+				AddItem (theObj);
+				theObj.SetActive (false);
+				waitItems.m_olapObjs.Remove ("PickUp");
+			}
+		}
+	}
+	public void AddItem (GameObject item)
+	{
+		if (!m_inventory.Contains (item)) {
 						
-						m_inventory.Add (invenNum, item);
-						invenNum += 1;
-				}
+			m_inventory.Add (item);
+			invenNum += 1;
 		}
-		public void RemoveSelItem ()
-		{
-				m_inventory.Remove (selected);
-				for (int index=selected; index<invenNum; index++) {
-						if (m_inventory.ContainsKey (index + 1)) {
-								//Debug.Log ("Replacing slot " + index + " with item at " + (index + 1));
-								m_inventory.Add (index, m_inventory [index + 1]);
-								m_inventory.Remove (index + 1);
-						}
-				}
-				selected = 0;
-				//Destroy()
+	}
+	public void RemoveSelItem ()
+	{
+		m_inventory.RemoveAt (m_numSelected);
+/*		for (int index=selected; index<invenNum; index++) {
+			if (m_inventory.ContainsKey (index + 1)) {
+				//Debug.Log ("Replacing slot " + index + " with item at " + (index + 1));
+				m_inventory.Add (index, m_inventory [index + 1]);
+				m_inventory.Remove (index + 1);
+			}
+		}*/
+		m_numSelected = 0;
+		//Destroy()
+	}
+	public void UseSelected ()
+	{
+		Usability useScript = getSelObj ().GetComponent<Usability> (); 
+		InvenObject chargeScript = getSelObj ().GetComponent<InvenObject> ();
+		if ((useScript != null) && (chargeScript != null)) {
+			useScript.Use (gameObject.transform);
+			chargeScript.decCharges ();
+			if (chargeScript.getCharges () <= 0) {
+				RemoveSelItem ();
+			}
 		}
-		public void UseSelected ()
-		{
-				Usability useScript = getSelObj ().GetComponent<Usability> (); 
-				InvenObject chargeScript = getSelObj ().GetComponent<InvenObject> ();
-				if ((useScript != null) && (chargeScript != null)) {
-						useScript.Use (gameObject.transform);
-						chargeScript.decCharges ();
-						if (chargeScript.getCharges () <= 0) {
-								RemoveSelItem ();
-						}
-				}
-		}
+	}
 /*		IEnumerator viewIven ()
 		{
 				yield return new WaitForSeconds (0.2f);
--- Assets/Scripts/Guis/InventoryGui.cs
+++ Assets/Scripts/Guis/InventoryGui.cs
@@ -3,32 +3,34 @@ using System.Collections;
 
 public class InventoryGui : MonoBehaviour
 {
-		public Texture2D btnTexture;
-		//public GUIStyle highlighted;
-		void OnGUI ()
-		{
-				GUI.Box (new Rect (170, Screen.height - 110, 1000, 100), "Inventory");
-				inventoryManager manager = gameObject.GetComponent<inventoryManager> ();
-				int x = 190;
-				int y = Screen.height - 90;
+	public Texture2D btnTexture;
+	//public GUIStyle highlighted;
+	void OnGUI ()
+	{
+		GUI.Box (new Rect (170, Screen.height - 110, 1000, 100), "Inventory");
+		inventoryManager manager = gameObject.GetComponent<inventoryManager> ();
+		int x = 190;
+		int y = Screen.height - 90;
 				
-				int index = 0;
-				foreach (var item in manager.getInven().Values) {
-						if (index == manager.getSelNum ()) {
-								GUI.color = Color.yellow;
-						} else {
-								GUI.color = Color.white;
-						}
-						if (GUI.Button (new Rect (x, y, 50, 50), btnTexture))
-								Debug.Log ("Clicked the button with an image");
-						GUI.Label (new Rect (x, y + 45, 50, 50), ((GameObject)item).name);
-						x = x + 60;
-						index += 1;
+		int i = 0;
+		if (manager.getInven ().Count > 0) {
+			foreach (GameObject item in manager.getInven()) {
+				if (i == manager.getSelNum ()) {
+					GUI.color = Color.yellow;
+				} else {
+					GUI.color = Color.white;
 				}
+				if (GUI.Button (new Rect (x, y, 50, 50), btnTexture))
+					Debug.Log ("Clicked the button with an image");
+				GUI.Label (new Rect (x, y + 45, 50, 50), ((GameObject)item).name);
+				x = x + 60;
+				i += 1;
+			}
+		}
 
 
 		
 				
 				
-		}
+	}
 }
