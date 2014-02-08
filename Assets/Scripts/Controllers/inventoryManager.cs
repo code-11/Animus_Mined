a98621f12c8fb7438c322c8cbc32c337f5a80d6a
Brendan Ritter <brendan.ritter@yahoo.com>
2014-02-08 14:05:18 -0500

    Fixed an IndexOutOfBounds error in the inventory system.

--- Assets/Scripts/Controllers/inventoryManager.cs
+++ Assets/Scripts/Controllers/inventoryManager.cs
@@ -4,116 +4,118 @@ using System.Collections;
 public class inventoryManager : MonoBehaviour
 {
 
-	/*
+		/*
 		m_inventory is a hashtable that remembers items in the player's inventory
 		Key: inventory number, ie which slot it is in.
 		Value: the gameobject itself.
 		*/
-	private ArrayList m_inventory;
-	//public GameObject m_placeholder;
-	private int invenNum = 0;
-	private bool invenUp = false;
-	private int m_numSelected = 0;
+		private ArrayList m_inventory;
+		//public GameObject m_placeholder;
+		private bool invenUp = false;
+		private int m_numSelected = 0;
 		
-	public GameObject getSelObj ()
-	{
-		return (GameObject)m_inventory [m_numSelected];
-	}
-	public void setSelNum (int newNum)
-	{
-		m_numSelected = newNum;
-	}
-	public int getSelNum ()
-	{
-		return m_numSelected;
-	}
-	void Start ()
-	{
+		public GameObject getSelObj ()
+		{
+				if (m_inventory.Count > 0) {
+						return (GameObject)m_inventory [m_numSelected];
+				} else {
+						return null;
+				}
+		}
+		public void setSelNum (int newNum)
+		{
+				m_numSelected = newNum;
+		}
+		public int getSelNum ()
+		{
+				return m_numSelected;
+		}
+		void Start ()
+		{
 /*				GameObject m_startItems[]={};*/
-		m_inventory = new ArrayList ();
+				m_inventory = new ArrayList ();
 /*				foreach (var item in m_startItems) {
 						m_inventory.Add (m_inventory.Count, item);
 				}*/
-	}
-	void Update ()
-	{
-		runInven ();
-	}
+		}
+		void Update ()
+		{
+				runInven ();
+		}
 		
-	void shiftLeft ()
-	{
-		if (m_numSelected == 0) {
-			m_numSelected = 0;
-		} else {
-			m_numSelected -= 1;
+		void shiftLeft ()
+		{
+				if (m_numSelected == 0) {
+						m_numSelected = 0;
+				} else {
+						m_numSelected -= 1;
+				}
 		}
-	}
-	void shiftRight ()
-	{
-		if (m_numSelected < m_inventory.Count - 1) {
-			m_numSelected += 1;
+		void shiftRight ()
+		{
+				if (m_numSelected < m_inventory.Count - 1) {
+						m_numSelected += 1;
+				}
 		}
-	}
 		
-	void runInven ()
-	{
-		InventoryGui invenGui = gameObject.GetComponent<InventoryGui> ();
-		bool iPress = Input.GetKeyDown ("i");
-		bool selLeft = Input.GetKeyDown ("q");
-		bool selRight = Input.GetKeyDown ("e");
-		bool rPress = Input.GetKeyDown ("r");
-		bool pPress = Input.GetKeyDown ("p");
-		if (pPress) {
-			RemoveSelItem ();
+		void runInven ()
+		{
+				InventoryGui invenGui = gameObject.GetComponent<InventoryGui> ();
+				bool iPress = Input.GetKeyDown ("i");
+				bool selLeft = Input.GetKeyDown ("q");
+				bool selRight = Input.GetKeyDown ("e");
+				bool rPress = Input.GetKeyDown ("r");
+				bool pPress = Input.GetKeyDown ("p");
+				if (pPress) {
+						RemoveSelItem ();
+				}
+				if (rPress) {
+						UseSelected ();
+				}
+				if (selLeft) {
+						shiftLeft ();
+				}
+				if (selRight) {
+						shiftRight ();
+				}
+				if (iPress) {
+						if (invenUp == true) {
+								invenUp = false;
+								invenGui.enabled = false;
+						} else {
+								invenUp = true;
+								invenGui.enabled = true;
+						}
+						//StartCoroutine (viewIven ());
+				}
+				pickUp ();				
+		}	
+		public ArrayList getInven ()
+		{
+				return m_inventory;
 		}
-		if (rPress) {
-			UseSelected ();
+		void pickUp ()
+		{
+				overlapDetector waitItems = gameObject.GetComponent<overlapDetector> ();
+				if (waitItems != null) {
+						if (waitItems.m_olapObjs.ContainsKey ("PickUp")) {
+								GameObject theObj = (GameObject)waitItems.m_olapObjs ["PickUp"];
+								AddItem (theObj);
+								theObj.SetActive (false);
+								waitItems.m_olapObjs.Remove ("PickUp");
+						}
+				}
 		}
-		if (selLeft) {
-			shiftLeft ();
-		}
-		if (selRight) {
-			shiftRight ();
-		}
-		if (iPress) {
-			if (invenUp == true) {
-				invenUp = false;
-				invenGui.enabled = false;
-			} else {
-				invenUp = true;
-				invenGui.enabled = true;
-			}
-			//StartCoroutine (viewIven ());
-		}
-		pickUp ();				
-	}	
-	public ArrayList getInven ()
-	{
-		return m_inventory;
-	}
-	void pickUp ()
-	{
-		overlapDetector waitItems = gameObject.GetComponent<overlapDetector> ();
-		if (waitItems != null) {
-			if (waitItems.m_olapObjs.ContainsKey ("PickUp")) {
-				GameObject theObj = (GameObject)waitItems.m_olapObjs ["PickUp"];
-				AddItem (theObj);
-				theObj.SetActive (false);
-				waitItems.m_olapObjs.Remove ("PickUp");
-			}
-		}
-	}
-	public void AddItem (GameObject item)
-	{
-		if (!m_inventory.Contains (item)) {
+		public void AddItem (GameObject item)
+		{
+				if (!m_inventory.Contains (item)) {
 						
-			m_inventory.Add (item);
-			invenNum += 1;
+						m_inventory.Add (item);
+				}
 		}
-	}
-	public void RemoveSelItem ()
-	{
-		m_inventory.RemoveAt (m_numSelected);
+		public void RemoveSelItem ()
+		{
+				m_inventory.RemoveAt (m_numSelected);
 /*		for (int index=selected; index<invenNum; index++) {
 			if (m_inventory.ContainsKey (index + 1)) {
 				//Debug.Log ("Replacing slot " + index + " with item at " + (index + 1));
@@ -121,21 +123,21 @@ public class inventoryManager : MonoBehaviour
 				m_inventory.Remove (index + 1);
 			}
 		}*/
-		m_numSelected = 0;
-		//Destroy()
-	}
-	public void UseSelected ()
-	{
-		Usability useScript = getSelObj ().GetComponent<Usability> (); 
-		InvenObject chargeScript = getSelObj ().GetComponent<InvenObject> ();
-		if ((useScript != null) && (chargeScript != null)) {
-			useScript.Use (gameObject.transform);
-			chargeScript.decCharges ();
-			if (chargeScript.getCharges () <= 0) {
-				RemoveSelItem ();
-			}
+				m_numSelected = 0;
+				//Destroy()
 		}
-	}
+		public void UseSelected ()
+		{
+				Usability useScript = getSelObj ().GetComponent<Usability> (); 
+				InvenObject chargeScript = getSelObj ().GetComponent<InvenObject> ();
+				if ((useScript != null) && (chargeScript != null)) {
+						useScript.Use (gameObject.transform);
+						chargeScript.decCharges ();
+						if (chargeScript.getCharges () <= 0) {
+								RemoveSelItem ();
+						}
+				}
+		}
 /*		IEnumerator viewIven ()
 		{
 				yield return new WaitForSeconds (0.2f);
