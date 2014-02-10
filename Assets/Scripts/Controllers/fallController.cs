c7f58b30f9856768061f3249c83e826645a497d6
Brendan Ritter <brendan.ritter@yahoo.com>
2014-02-09 22:15:41 -0500

    Fixed snapping behavior when falling.
    Was one line in fall controller. Pretty sure it was there for a reason,
    but despite tests, couldn't notice its removal.

--- Assets/Scripts/Controllers/fallController.cs
+++ Assets/Scripts/Controllers/fallController.cs
@@ -55,7 +55,7 @@ public class fallController : MonoBehaviour
 				Vector3 amountMove = (new Vector3 (0, m_gravity, 0)) * Time.fixedDeltaTime;
 		
 				bool collidersNull = downInfoLeft.collider == null && downInfoRight.collider == null;
-				if ((leastDiff <= -amountMove.y) || (!collidersNull)) {
+				if ((leastDiff <= -amountMove.y) /*|| (!collidersNull)*/) {
 						transform.position += (new Vector3 (0, -leastDiff, 0));
 				} else if ((collidersNull) || (leastDiff >= -amountMove.y)) {
 						transform.position += amountMove;
