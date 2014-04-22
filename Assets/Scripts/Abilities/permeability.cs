91eb80b954fd642e89ddc1dd0768b83cc4eb6635
Brendan Ritter <brendan.ritter@yahoo.com>
2014-04-21 22:47:00 -0400

    Generalized permeability
    Also got rid of a lot of warnings. DON'T TRY TO FIX THE ONE THAT IS
    LEFT. That field is actually used but I think by another class. Also
    moved some obsolete scripts to their own folders.

--- Assets/Scripts/Abilities/permeability.cs
+++ Assets/Scripts/Abilities/permeability.cs
@@ -1,11 +1,28 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class permeability : MonoBehaviour
 {
-		public bool m_horizontal;
-		//A player can always leave a block. However, in order to enter a block, requires these.
-		public bool m_throughTop;
-		public bool m_throughBottom;
-	
+	public bool m_throughTop;
+	public bool m_throughBottom;
+	public bool m_throughLeft;
+	public bool m_throughRight;
+	public bool getUp ()
+	{
+		return m_throughTop;
+	}
+	public bool getDown ()
+	{
+		return m_throughBottom;
+	}
+	public bool getLeft ()
+	{
+		return m_throughLeft;
+	}
+	public bool getRight ()
+	{
+		return m_throughRight;
+	}
+	//For legacy reasons
+	public bool m_horizontal; 
 }
