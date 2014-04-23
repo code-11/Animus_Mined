6a6d6929fe3c8306d827d756d8fc1e95274020d6
Brendan Ritter <brendan.ritter@yahoo.com>
2014-04-22 22:28:01 -0400

    Implemented drops occurring on a percentage chance.
    Also re added player death due to falling objects.

--- Assets/Scripts/Abilities/mineability.cs
+++ Assets/Scripts/Abilities/mineability.cs
@@ -8,11 +8,18 @@ public class mineability : MonoBehaviour
 		public GameObject m_drop;
 		public int m_numDrop;
 		public int m_health = 100;
+		public int m_percentChance = 100;
+
+		bool doesDrop ()
+		{
+				int rand = Random.Range (0, 100);
+				return (rand < m_percentChance);
+		}
 
 		void Update ()
 		{
 				if (m_health <= 0) {
-						if (m_drop != null) {
+						if ((m_drop != null) && (doesDrop ())) {
 								GameObject theDrop = (GameObject)Instantiate (m_drop, new Vector3 (transform.position.x, transform.position.y, 0), Quaternion.identity);
 								InvenObject inven = theDrop.GetComponent<InvenObject> ();
 								inven.setCharges (m_numDrop);
--- Assets/Scripts/Controllers/discreteMovement.cs
+++ Assets/Scripts/Controllers/discreteMovement.cs
@@ -136,6 +136,13 @@ public class discreteMovement : MonoBehaviour
 				
 		}
 		
+		void killSelf ()
+		{
+				Time.timeScale = 0.3F;
+				Destroy (this.gameObject);
+				Debug.Log ("Game Over");
+		}
+	
 		void makeMove (bool up, bool down, bool left, bool right)
 		{
 				Vector2 pos = transform.position;
