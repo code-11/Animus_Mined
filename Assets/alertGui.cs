6775d850a1c61d1a329b525f6853e2e4535eab99
Brendan Ritter <brendan.ritter@yahoo.com>
2015-01-01 13:52:19 -0500

    Started on alert system. Started on research tree.

---
+++ Assets/alertGui.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class alertGui : MonoBehaviour
+{
+
+
+		public string m_alert;
+		// Use this for initialization
+		void Start ()
+		{
+				m_alert = "";
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+		void OnGUI ()
+		{
+				// Make a background box
+				int buffer = 20;
+				GUI.Box (new Rect (buffer, Screen.height - 2 * buffer, Screen.width - 2 * buffer, 30), m_alert);					
+		
+				//Debug.Log (Screen.height);
+		}
+		void setAlert (string newAlert)
+		{
+				m_alert = newAlert;
+		}
+}
