1a0cdb65a2034679b714af98a35a304bd16ec525
Brendan Ritter <brendan.ritter@yahoo.com>
2015-02-25 13:26:06 -0500

    Made death manager.
    When you die it looks for a replicator and respawns you at it.

--- Assets/Scripts/Controllers/deathController.cs
+++ Assets/Scripts/Controllers/deathController.cs
@@ -2,16 +2,27 @@
 using System.Collections;
 
 public class deathController : MonoBehaviour {
+	private alertManager alertCtrl;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+		alertCtrl=gameObject.GetComponent<alertManager>();
+
+	} 
 	void killSelf ()
 	{
-		Time.timeScale = 0.3F;
-		Destroy (this.gameObject);
-		Debug.Log ("Game Over");
+		GameObject a_replicator=GameObject.Find("prefabReplicator");
+		if (a_replicator!=null){
+			alertCtrl.setAlert("Uploading Neural Net");
+			gameObject.transform.position=a_replicator.transform.position;
+			replicatorController repr =a_replicator.GetComponent<replicatorController>();
+			repr.lessBody();
+		}else{
+			Time.timeScale = 0.3F;
+			Destroy (this.gameObject);
+			alertCtrl.setAlert("Backing up Consciousness. Sending Emergency Beacon.");
+			Debug.Log ("Game Over");
+		}
 	}
 	
 	// Update is called once per frame
---
+++ Assets/replicatorController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class replicatorController : MonoBehaviour {
+
+	private int m_bodies=3;
+	public void lessBody(){
+		m_bodies-=1;
+		if (m_bodies<=0){
+			Destroy (this.gameObject);
+		}
+	}
+}
