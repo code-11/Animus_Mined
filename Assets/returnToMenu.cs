b733e2cc494238d69ae87d63f8298c58a427de7a
Brendan Ritter <brendan.ritter@yahoo.com>
2015-03-07 13:03:45 -0500

    Added delay to death screen.

--- Assets/returnToMenu.cs
+++ Assets/returnToMenu.cs
@@ -2,15 +2,22 @@
 using System.Collections;
 
 public class returnToMenu : MonoBehaviour {
-
+	public float m_delay;
+	private bool m_delaying;
 	// Use this for initialization
 	void Start () {
-	
+		m_delay=4f;
+		StartCoroutine (menuDelay());
+	}
+	IEnumerator menuDelay(){
+		m_delaying=true;
+		yield return new WaitForSeconds (m_delay);
+		m_delaying=false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.anyKey){
+		if (Input.anyKey && (m_delaying==false)){
 			Application.LoadLevel("startingScene");
 		}
 	}
