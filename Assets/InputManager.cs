716a8a9e21c029a2c08f9eb842106468a8bd00ea
Brendan Ritter <brendan.ritter@yahoo.com>
2015-05-12 23:56:09 -0700

    Added optional console control

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -3,60 +3,78 @@ using System.Collections;
 
 public class InputManager : MonoBehaviour {
 
+	bool m_horiz=false;
+	bool m_vertUp=false;
+	bool m_vertDown=false;
+
+	public bool getAxisDown(bool theLock,string axis){
+				if (Input.GetAxis (axis) != 0) {
+					if (theLock == false) {
+							return true;
+							theLock = true;
+						}
+				} else {
+						theLock = false;
+				}
+		return false;
+	}
+	public void Update(){
+	}
+
 	public bool upMovePress(){
-		return Input.GetKey("w");
+		return (Input.GetKey("w")||Input.GetButton ("Up"));
 	}
 	public bool downMovePress(){
-		return Input.GetKey("s");
+		return (Input.GetKey("s")||Input.GetButton ("Down"));
 	}
 	public bool leftMovePress(){
-		return Input.GetKey("a");
+		return (Input.GetKey("a")||Input.GetButton ("Left"));
 	}
 	public bool rightMovePress(){
-		return Input.GetKey("d");
+		return (Input.GetKey("d")||Input.GetButton ("Right"));
 	}
 	public bool guiUpPress(){
-		return Input.GetKeyDown("w");
+		return (Input.GetKeyDown("w")||Input.GetButtonDown ("Up"));
 	}
 	public bool guiDownPress(){
-		return Input.GetKeyDown("s");
+		return (Input.GetKeyDown("s")||Input.GetButtonDown ("Down"));
 	}
 	public bool guiLeftPress(){
-		return Input.GetKeyDown("a");
+		return (Input.GetKeyDown("a")||Input.GetButtonDown ("Left"));
 	}
 	public bool guiRightPress(){
-		return Input.GetKeyDown("d");
+		return (Input.GetKeyDown("d")||Input.GetButtonDown ("Right"));
 	}
 
 
 
 
 	public bool upMinePress(){
-		return Input.GetKey("up");
+		return (Input.GetKey("up")||Input.GetAxis ("Vertical")>0);
 	}
 	public bool downMinePress(){
-		return Input.GetKey("down");
+		return (Input.GetKey("down")||Input.GetAxis ("Vertical")<0);
 	}
 	public bool leftMinePress(){
-		return Input.GetKey("left");
+		return (Input.GetKey("left")||Input.GetAxis ("Horizontal")<0);
 	}
 	public bool rightMinePress(){
-		return Input.GetKey("right");
+		return (Input.GetKey("right")||Input.GetAxis ("Horizontal")>0);
 	}
 	public bool invenLeftPress(){
-		return Input.GetKeyDown("q");
+		return Input.GetKeyDown("q")||Input.GetButtonDown ("LeftInven");
 	}
 	public bool invenRightPress(){
-		return Input.GetKeyDown("e");
+		return Input.GetKeyDown("e")||Input.GetButtonDown ("RightInven");
 	}
 	public bool invenPress(){
 		return Input.GetKeyDown("i");
 	}
 	public bool usePress(){
-		return Input.GetKeyDown("r");
+		return (Input.GetKeyDown("r")||Input.GetButtonDown ("Use"));
 	}
 	public bool useBuildPress(){
-		return Input.GetKeyDown("f");
+		return (Input.GetKeyDown("f")||Input.GetButtonDown ("Use2"));
 	}
 	public bool dropPress(){
 		return Input.GetKeyDown("p");
@@ -65,6 +83,6 @@ public class InputManager : MonoBehaviour {
 		return Input.GetKeyDown("c");
 	}
 	public bool escPress(){
-		return Input.GetKeyDown("escape");
+		return Input.GetKeyDown("escape")||Input.GetButtonDown("Esc");
 	}
 }
